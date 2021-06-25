fe82ebda25419b53eee44882a107aea5ddc343d8
Mate Kezy <kezymate@gmail.com>
2021-06-25 18:25:48 +0200

    fix: sign up with email page

--- apps/mobile_app/lib/sign-up-with-email-page.dart
+++ apps/mobile_app/lib/sign-up-with-email-page.dart
@@ -65,12 +65,24 @@ class _SignUpWithEmailPageState extends State<SignUpWithEmailPage> {
                               child: SizedBox(
                                 height: 40.0,
                                 child: TextFormField(
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      color: YahaColors.textColor,
+                                      fontSize: YahaFontSizes.small),
                                   decoration: InputDecoration(
-                                      border: OutlineInputBorder(
+                                      contentPadding: EdgeInsets.only(
+                                          left: YahaSpaceSizes.medium),
+                                      enabledBorder: OutlineInputBorder(
                                           borderRadius: BorderRadius.circular(
                                               YahaBorderRadius.small),
                                           borderSide: BorderSide(
                                               color: YahaColors.textColor,
+                                              width: YahaBorderWidth.small)),
+                                      focusedBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(
+                                              YahaBorderRadius.small),
+                                          borderSide: BorderSide(
+                                              color: YahaColors.secondary,
                                               width: YahaBorderWidth.small))),
                                 ),
                               ),
@@ -78,7 +90,211 @@ class _SignUpWithEmailPageState extends State<SignUpWithEmailPage> {
                           ],
                         ),
                       ),
-                    )
+                    ),
+                    SizedBox(
+                      width: 300.0,
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Email',
+                              style: TextStyle(
+                                  fontSize: YahaFontSizes.small,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(
+                                top: YahaSpaceSizes.xSmall,
+                                bottom: YahaSpaceSizes.medium),
+                            child: SizedBox(
+                              height: 40.0,
+                              child: TextFormField(
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    color: YahaColors.textColor,
+                                    fontSize: YahaFontSizes.small),
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.only(
+                                        left: YahaSpaceSizes.medium),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(
+                                            YahaBorderRadius.small),
+                                        borderSide: BorderSide(
+                                            color: YahaColors.textColor,
+                                            width: YahaBorderWidth.small)),
+                                    focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(
+                                            YahaBorderRadius.small),
+                                        borderSide: BorderSide(
+                                            color: YahaColors.secondary,
+                                            width: YahaBorderWidth.small))),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 300.0,
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Password',
+                              style: TextStyle(
+                                  fontSize: YahaFontSizes.small,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(
+                                top: YahaSpaceSizes.xSmall,
+                                bottom: YahaSpaceSizes.xSmall),
+                            child: SizedBox(
+                              height: 40.0,
+                              child: TextFormField(
+                                obscureText: true,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    color: YahaColors.textColor,
+                                    fontSize: YahaFontSizes.small),
+                                decoration: InputDecoration(
+                                    suffixIcon: Icon(
+                                      Icons.visibility_outlined,
+                                      color: YahaColors.textColor,
+                                    ),
+                                    contentPadding: EdgeInsets.only(
+                                        left: YahaSpaceSizes.medium),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(
+                                            YahaBorderRadius.small),
+                                        borderSide: BorderSide(
+                                            color: YahaColors.textColor,
+                                            width: YahaBorderWidth.small)),
+                                    focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(
+                                            YahaBorderRadius.small),
+                                        borderSide: BorderSide(
+                                            color: YahaColors.secondary,
+                                            width: YahaBorderWidth.small))),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 300.0,
+                            child: Container(
+                              padding: const EdgeInsets.only(
+                                  bottom: YahaSpaceSizes.medium),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('At least 8 characters long'),
+                                  Text('Contains numbers'),
+                                  Text('Contains special characters')
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 300.0,
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Password again',
+                              style: TextStyle(
+                                  fontSize: YahaFontSizes.small,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(
+                                top: YahaSpaceSizes.xSmall,
+                                bottom: YahaSpaceSizes.large),
+                            child: SizedBox(
+                              height: 40.0,
+                              child: TextFormField(
+                                obscureText: true,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    color: YahaColors.textColor,
+                                    fontSize: YahaFontSizes.small),
+                                decoration: InputDecoration(
+                                    suffixIcon: Icon(
+                                      Icons.visibility_outlined,
+                                      color: YahaColors.textColor,
+                                    ),
+                                    contentPadding: EdgeInsets.only(
+                                        left: YahaSpaceSizes.medium),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(
+                                            YahaBorderRadius.small),
+                                        borderSide: BorderSide(
+                                            color: YahaColors.textColor,
+                                            width: YahaBorderWidth.small)),
+                                    focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(
+                                            YahaBorderRadius.small),
+                                        borderSide: BorderSide(
+                                            color: YahaColors.secondary,
+                                            width: YahaBorderWidth.small))),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: SizedBox(
+                        width: 300.0,
+                        height: 50.0,
+                        child: ElevatedButton(
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(
+                                fontSize: YahaFontSizes.small,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: YahaColors.primary,
+                            shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                    Radius.circular(YahaBorderRadius.general))),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      //child: Align(
+                      //alignment: FractionalOffset.bottomCenter,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Already have an account? ',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: YahaFontSizes.small)),
+                          Text('Log in',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: YahaColors.primary,
+                                  fontSize: YahaFontSizes.small)),
+                        ],
+                      ),
+                    ),
+                    //),
                   ],
                 );
               },
