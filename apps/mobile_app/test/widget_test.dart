f23ef721adc5fce30fb2b644cddfafff8d4a065c
Nikolett Szimeonov <szimi7@gmail.com>
2021-06-29 10:46:00 +0200

    fix: yaha smoke tests

--- apps/mobile_app/test/widget_test.dart
+++ apps/mobile_app/test/widget_test.dart
@@ -8,7 +8,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile_app/bottom-nav-bar.dart';
 import 'package:mobile_app/event-detail-page.dart';
+import 'package:mobile_app/gallery.dart';
 import 'package:mobile_app/hike-page.dart';
 
 import 'package:mobile_app/main.dart';
@@ -16,10 +18,11 @@ import 'package:mobile_app/poi-page.dart';
 import 'package:mobile_app/profile-page.dart';
 import 'package:mobile_app/settings-page.dart';
 import 'package:mobile_app/sign-up-page.dart';
+import 'package:mobile_app/sign-up-with-email-page.dart';
+import 'package:mobile_app/track-page.dart';
 
 void main() {
   testWidgets('Yaha main smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
     expect(true, true);
   });
@@ -55,9 +58,30 @@ void main() {
 
     await tester.pumpAndSettle();
   });
+   testWidgets('Sign Up With Email Page smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(createWidgetForTesting(child: new SignUpWithEmailPage()));
+
+    await tester.pumpAndSettle();
+  });
   testWidgets('Event Detail Page smoke test', (WidgetTester tester) async {
     await tester
         .pumpWidget(createWidgetForTesting(child: new EventDetailPage()));
+
+    await tester.pumpAndSettle();
+  });
+    testWidgets('Track Page smoke test', (WidgetTester tester) async {
+    await tester
+        .pumpWidget(createWidgetForTesting(child: new TrackPage()));
+
+    await tester.pumpAndSettle();
+  });
+  testWidgets('Gallery smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(createWidgetForTesting(child: new Gallery()));
+
+    await tester.pumpAndSettle();
+  });
+    testWidgets('Bottom nav bar smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(createWidgetForTesting(child: new BottomNavBar()));
 
     await tester.pumpAndSettle();
   });
