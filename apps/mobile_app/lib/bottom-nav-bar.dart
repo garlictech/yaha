c0761039999da7cfd85fd06d288489dd412f6e4b
Mate Kezy <kezymate@gmail.com>
2021-08-03 12:19:52 +0200

    feat: hike card init

--- apps/mobile_app/lib/bottom-nav-bar.dart
+++ apps/mobile_app/lib/bottom-nav-bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yaha/hike-page.dart';
 import 'package:yaha/log-in-page.dart';
+import 'package:yaha/previous-activities-page.dart';
 import 'package:yaha/profile-page.dart';
 import 'package:yaha/yaha-colors.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
@@ -52,7 +53,7 @@ class BottomNavBar extends StatelessWidget {
 }
 
 List<Widget> _buildScreens() {
-  return [HikePage(), ExplorePage(), LogInPage(), ProfilePage()];
+  return [HikePage(), ExplorePage(), PreviousActivitiesPage(), ProfilePage()];
 }
 
 List<PersistentBottomNavBarItem> _navBarsItems() {
---
+++ apps/mobile_app/lib/previous-activities-page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:yaha/hike/hike-card-list.dart';
+import 'package:yaha/yaha-colors.dart';
+import 'package:yaha/yaha-font-sizes.dart';
+import 'package:yaha/yaha-space-sizes.dart';
+
+class PreviousActivitiesPage extends StatefulWidget {
+  @override
+  _PreviousActivitiesState createState() => _PreviousActivitiesState();
+}
+
+class _PreviousActivitiesState extends State<PreviousActivitiesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: <Widget>[
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return SafeArea(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(
+                            bottom: YahaSpaceSizes.general),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            IconButton(
+                              iconSize: YahaFontSizes.xxLarge,
+                              icon: Icon(
+                                Icons.arrow_back_ios_rounded,
+                                color: YahaColors.textColor,
+                              ),
+                              onPressed: () {},
+                            ),
+                            Text(
+                              'Previous activities',
+                              style: TextStyle(
+                                  fontSize: YahaFontSizes.large,
+                                  fontWeight: FontWeight.w600,
+                                  color: YahaColors.textColor),
+                            ),
+                            IconButton(
+                              icon: Image.asset(
+                                'assets/images/filter-icon.png',
+                                width: 28.0,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        //height: MediaQuery.of(context).size.height,
+                        height: 800,
+                        child: HikeCardList(),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
