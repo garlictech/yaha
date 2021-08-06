631f64dc21da62994fd5fc18334de2f8a0f58404
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-08-06 16:14:20 +0200

    chore(cicd): display current node version

--- tools/install-nodejs-14.sh
+++ tools/install-nodejs-14.sh
@@ -9,11 +9,4 @@ curl -O https://nodejs.org/download/release/$version/$package_name.tar.gz
 tar -xvf $package_name.tar.gz -C $package_location
 rm -rfv $package_name.tar.gz
 
-echo "export PATH=$package_location/$package_name/bin:\$PATH" >> ~/.profile
-echo "export PATH=$package_location/$package_name/bin:\$PATH" >> ~/.bashrc
-
-#export PATH=$package_location/$package_name/bin:\$PATH
-node --version
-which node
 ln -sf $package_location/$package_name/bin/node `which node`
-node --version
