7770024e3fd67de0f22a842c8eb6f761c633f1ce
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-08-12 14:56:39 +0200

    fix(cicd): pr build fix

--- apps/cicd/scripts/pr-build.sh
+++ apps/cicd/scripts/pr-build.sh
@@ -10,7 +10,7 @@ generatedLibExcludes="--exclude=shared-config --exclude=yaha-gql-api --exclude=c
 #yarn nx format:check 
 yarn nx affected:lint --base=${STAGE} ${generatedLibExcludes}
 yarn nx affected:test --base=${STAGE} --exclude="integration-tests-universal" --exclude="mobile_app" ${generatedLibExcludes} --codeCoverage --coverageReporters=clover
-yarn nx lint mobile_app
+yarn nx lint-ci mobile_app
 yarn nx test-ci mobile_app
 yarn nx buildApk-ci mobile_app
 npx cowsay "YOUR PR IS SUPERCOOL!!!"
