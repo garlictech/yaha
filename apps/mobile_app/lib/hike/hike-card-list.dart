6325a0ba8117ce12e4cf523ca94bef43f5999afe
Máté Kézy <63058380+kezymate@users.noreply.github.com>
2021-09-02 17:02:27 +0200

    Guest home page init (#292)
    * feat: home page guest init
    
    * fix: remove unused import
    
    * fix: conflict
    
    * fix: yarn frozen lockfile
    
    * fix(cicd): schema checker lib path changed
    
    * fix(schema): schema fix
    
    * fix: podfile
    
    * fix(schema): schema fix
    
    * fix(mobile_app): fix a failed unit test
    
    * feat(mobile_app): header resdponds to login
    
    * fix: current challenges
    
    * fix: finish guest home screen
    
    * fix: hike card optional parameter
    
    * fix: remove constants
    
    * fix: challenge box overflow
    
    Co-authored-by: Zsolt Molnar <zsolt@zsoltmolnar.hu>
    Co-authored-by: Máté Kézy <matekezy@Mates-MacBook-Pro-ceges.local>

--- apps/mobile_app/lib/hike/hike-card-list.dart
+++ apps/mobile_app/lib/hike/hike-card-list.dart
@@ -15,27 +15,27 @@ final List<String> titles = [
 ];
 
 final data = [
-  ['Hike 1', 'Hike 1 subtitle', 220],
-  ['Hike 2', 'Hike 2 subtitle', 220],
-  ['Hike 3', 'Hike 3 subtitle', 220],
-  ['Hike 4', 'Hike 4 subtitle', 220],
-  ['Hike 5', 'Hike 1 subtitle', 220],
-  ['Hike 6', 'Hike 2 subtitle', 220],
-  ['Hike 7', 'Hike 3 subtitle', 220],
-  ['Hike 8', 'Hike 4 subtitle', 220],
-  ['Hike 9', 'Hike 1 subtitle', 220],
-  ['Hike 10', 'Hike 2 subtitle', 220],
-  ['Hike 11', 'Hike 3 subtitle', 220],
-  ['Hike 12', 'Hike 4 subtitle', 220],
+  ['Hike 1', 'Hike 1 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 2', 'Hike 2 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 3', 'Hike 3 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 4', 'Hike 4 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 5', 'Hike 1 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 6', 'Hike 2 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 7', 'Hike 3 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 8', 'Hike 4 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 9', 'Hike 1 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 10', 'Hike 2 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 11', 'Hike 3 subtitle', 220, 'assets/images/Parlament.jpg'],
+  ['Hike 12', 'Hike 4 subtitle', 220, 'assets/images/Parlament.jpg'],
 ];
 
 final images = data
     .map((e) => HikeCard(
           //color: e[0] as Color,
-
           title: e[0] as String,
           subTitle: e[1] as String,
           distanceFromCurrentLocation: e[2] as int,
+          backgroundImage: e[3] as String,
         ))
     .toList();
 
---
+++ apps/mobile_app/lib/utility/challenge-box.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:yaha/utility/yaha-border-radius.dart';
+import 'package:yaha/utility/yaha-border-width.dart';
+import 'package:yaha/utility/yaha-box-sizes.dart';
+import 'package:yaha/utility/yaha-colors.dart';
+import 'package:yaha/utility/yaha-font-sizes.dart';
+import 'package:yaha/utility/yaha-image-sizes.dart';
+import 'package:yaha/utility/yaha-space-sizes.dart';
+
+class ChallengeBox extends StatefulWidget {
+  final String title;
+  final String icon;
+
+  const ChallengeBox({
+    Key? key,
+    required this.title,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  _ChallengeBoxState createState() => _ChallengeBoxState();
+}
+
+class _ChallengeBoxState extends State<ChallengeBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: YahaBoxSizes.widthGeneral,
+      //height: YahaBoxSizes.heightGeneral,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: YahaColors.primary,
+          width: YahaBorderWidth.medium,
+        ),
+        borderRadius: BorderRadius.circular(YahaBorderRadius.general),
+      ),
+      child: Container(
+        padding: EdgeInsets.all(YahaSpaceSizes.general),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              widget.icon,
+              width: YahaImagesSizes.widthSmall,
+            ),
+            Text(
+              widget.title,
+              style: TextStyle(
+                color: YahaColors.primary,
+                fontSize: YahaFontSizes.xSmall,
+                fontWeight: FontWeight.w400,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
