481405292a8a0691dd84f988f6824686334737aa
Máté Kézy <63058380+kezymate@users.noreply.github.com>
2021-09-09 12:13:35 +0200

    Hike outline init (#390)
    * fix: podfile
    
    * feat: hike outline page init
    
    Co-authored-by: Máté Kézy <matekezy@Mates-MacBook-Pro-ceges.local>

---
+++ apps/mobile_app/lib/hike-outline/hike-outline-page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:yaha/utility/buttons/back-button.dart';
+import 'package:yaha/utility/buttons/filter-button.dart';
+import 'package:yaha/utility/buttons/settings-button.dart';
+import 'package:yaha/utility/yaha-colors.dart';
+import 'package:yaha/utility/yaha-font-sizes.dart';
+import 'package:yaha/utility/yaha-space-sizes.dart';
+
+class HikeOutlinePage extends StatefulWidget {
+  @override
+  _HikeOutlineState createState() => _HikeOutlineState();
+}
+
+class _HikeOutlineState extends State<HikeOutlinePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: <Widget>[
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return SafeArea(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(
+                          left: YahaSpaceSizes.medium,
+                          right: YahaSpaceSizes.medium,
+                          top: YahaSpaceSizes.small,
+                        ),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: YahaBackButton(),
+                            ),
+                            Align(
+                              alignment: Alignment.center,
+                              child: Text('Hike Outline',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: YahaFontSizes.large,
+                                      fontWeight: FontWeight.w600,
+                                      color: YahaColors.textColor)),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: SettingsButton(),
+                            ),
+                            Positioned(
+                              right: 55.0,
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: FilterButton(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
