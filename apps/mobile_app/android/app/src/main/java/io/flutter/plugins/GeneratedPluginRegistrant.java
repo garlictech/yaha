d626b2b49556d35cd3e1649848e11e2ae27296ca
Máté Kézy <matekezy@Mates-MacBook-Pro-ceges.local>
2021-10-29 13:55:53 +0200

    feat: signup confirmation screen

--- apps/mobile_app/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ apps/mobile_app/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -13,10 +13,6 @@ import io.flutter.embedding.engine.FlutterEngine;
 @Keep
 public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
-    flutterEngine.getPlugins().add(new com.amazonaws.amplify.amplify_auth_cognito.AuthCognito());
-    flutterEngine.getPlugins().add(new com.amazonaws.amplify.amplify_core.AmplifyCorePlugin());
-    flutterEngine.getPlugins().add(new com.amazonaws.amplify.amplify_datastore.AmplifyDataStorePlugin());
-    flutterEngine.getPlugins().add(new com.amazonaws.amplify.Amplify());
     flutterEngine.getPlugins().add(new com.linusu.flutter_web_auth.FlutterWebAuthPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin());
