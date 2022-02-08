a6322c8d2df7dc7f9406acb98583102778031fe8
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-02-08 06:00:17 +0100

    chore(mobile_app): build fix

--- apps/mobile_app/lib/repositories/hikes/hike-repository-amplify.dart
+++ apps/mobile_app/lib/repositories/hikes/hike-repository-amplify.dart
@@ -4,19 +4,118 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yaha/models/ModelProvider.dart';
 import 'hike-repository.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_api/amplify_api.dart';
 
 class HikeRepositoryAmplify implements HikeRepository {
   @override
   getHikeList() async {
+    String gqlDocument = ''' 
+          query ListHikes {
+    listHikes {
+      items {
+        id
+        description {
+          languageKey
+          title
+          summary
+          fullDescription
+          type
+        }
+        location
+        featured
+        rate
+        offlineMap
+        featuredStops {
+          poiId
+          description {
+            languageKey
+            title
+            summary
+            fullDescription
+            type
+          }
+        }
+        checkpoints {
+          id
+          location {
+            lat
+            lon
+            ele
+          }
+          types
+          description {
+            languageKey
+            title
+            summary
+            fullDescription
+            type
+          }
+          tags
+          sourceObject {
+            objectType
+            languageKey
+            objectId
+            url
+          }
+          address
+          phoneFloat
+          openingHours
+          createdAt
+          updatedAt
+          _version
+          _deleted
+          _lastChangedAt
+        }
+        tags
+        route {
+          type
+          coordinates
+        }
+        startPoint {
+          lat
+          lon
+          ele
+        }
+        endPoint {
+          lat
+          lon
+          ele
+        }
+        imageUrls
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+      }
+      nextToken
+      startedAt
+    }
+  }
+  ''';
     try {
-      // Todo: wait for proper amplify initialization
-      await Future.delayed(const Duration(seconds: 10));
-      await Amplify.DataStore.query(Hike.classType);
+      debugPrint('HERE');
+      var request = GraphQLRequest<String>(document: gqlDocument);
+      var operation = Amplify.API.query(request: request);
+      var response = await operation.response;
+      debugPrint('HERE2');
+      var data = response.data;
+      debugPrint(data.toString());
       return [];
+    } on ApiException catch (e) {
+      debugPrint('Query failed: $e');
+      return [];
+    }
+    /*try {
+      // Todo: wait for proper amplify initialization
+      await Future.delayed(const Duration(seconds: 2));
+      return await Amplify.DataStore.query(Hike.classType);
     } on DataStoreException catch (e) {
       debugPrint('Hike query failed: $e');
       throw ('Hike query failed: $e');
     }
+    */
   }
 }
 
