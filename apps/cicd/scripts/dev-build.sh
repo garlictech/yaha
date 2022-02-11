0c06264b8c05b193d488f536718ecc04223e2b1e
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-02-11 10:31:35 +0100

    chore(app): ci build fix

--- apps/cicd/scripts/dev-build.sh
+++ apps/cicd/scripts/dev-build.sh
@@ -6,9 +6,9 @@ CI=$2
 
 ./tools/build-workspace.sh $ENVNAME $CI
 
-if [ "$ENVNAME" = "dev" ]; then
-  yarn nx deploy common-backend --env=${ENVNAME}
-fi
+#if [ "$ENVNAME" = "dev" ]; then
+#  yarn nx deploy common-backend --env=${ENVNAME}
+#fi
 
 yarn nx deploy-amplify mobile_app --env=${ENVNAME}
 yarn nx deploy backend --env=${ENVNAME}
