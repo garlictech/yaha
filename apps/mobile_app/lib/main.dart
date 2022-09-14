e23d9c1be996fb47f50758e1ebdfe10b8e605388
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-09-14 23:52:52 +0200

    chore(deps): flutter upgrade

--- apps/mobile_app/lib/main.dart
+++ apps/mobile_app/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yaha/ui/views/auth/screens/signup-screen.dart';
@@ -7,27 +6,12 @@ import 'package:yaha/ui/views/hikes/personal/screen/profile-page.dart';
 import 'package:yaha/ui/views/hikes/personal/screen/settings-screen.dart';
 import 'package:yaha/ui/views/shared/widgets/yaha-colors.dart';
 import 'package:yaha/ui/views/hikes/track/screens/tracking.dart';
-import 'amplifyconfiguration.dart';
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 
 import 'ui/views/shared/widgets/bottom-nav-bar-widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initialization();
   runApp(const ProviderScope(child: MyApp()));
-}
-
-initialization() async {
-  // Add the following lines to your app initialization to add the DataStore plugin
-  try {
-    await Amplify.addPlugins([AmplifyAuthCognito()]);
-    await Amplify.configure(amplifyconfig);
-    debugPrint("Amplify config done");
-  } on AmplifyAlreadyConfiguredException {
-    debugPrint(
-        "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
-  }
 }
 
 class MyApp extends StatefulWidget {
