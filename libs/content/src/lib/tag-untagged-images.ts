ba485f701a1b908a95996f8b4522295ac5af38fb
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-09-17 22:31:55 +0200

    fix: mor stable waypoint query

--- libs/content/src/lib/tag-untagged-images.ts
+++ libs/content/src/lib/tag-untagged-images.ts
@@ -1,23 +1,51 @@
 import { pipe } from 'fp-ts/lib/function';
 import { Neo4jdeps } from './utils';
 import * as R from 'ramda';
-import { of, defer, from } from 'rxjs';
-import { map, concatMap, switchMap, tap, count } from 'rxjs/operators';
+import { of, defer, from, throwError } from 'rxjs';
+import {
+  map,
+  retry,
+  concatMap,
+  switchMap,
+  tap,
+  takeWhile,
+  count,
+  catchError,
+} from 'rxjs/operators';
 import { tagImage } from '@yaha/backend/image-tagger';
 import * as OE from 'fp-ts-rxjs/lib/ObservableEither';
 
-export const tagUntaggedImages = (deps: Neo4jdeps) =>
+const poiQuery = `
+match (i:Image {banned: false})-[:TAKEN_AT]->(p:Poi) 
+with p, count(i) as cnt
+where cnt < 11
+match (i:Image)-[:TAKEN_AT]->(p) where i.processed is null
+with p, cnt, count(i) as pcnt, i
+where pcnt > 0
+return i limit 1
+`;
+
+const hikeQuery = `
+match (i:Image {banned: false})-[:TAKEN_AT]->(p:Hike) 
+with p, count(i) as cnt
+where cnt < 31
+match (i:Image)-[:TAKEN_AT]->(p) where i.processed is null
+with p, cnt, count(i) as pcnt, i
+where pcnt > 0
+return i limit 1
+`;
+
+export const tagUntaggedImagesOfEntity = (deps: Neo4jdeps) => (query: string) =>
   pipe(
-    `
-match (image:Image) where image.processed is null
-return image limit 1000 
-`,
-    query => defer(() => deps.session.readTransaction(tx => tx.run(query))),
+    defer(() => deps.session.readTransaction(tx => tx.run(query))),
     map(result =>
       pipe(
         result.records,
         R.map(record => record.get(0).properties),
-        R.map(props => ({ url: props.card, id: props.id })),
+        R.map(props => ({
+          url: props.card,
+          id: props.id,
+        })),
       ),
     ),
     switchMap(x => from(x)),
@@ -52,4 +80,11 @@ set image.processed = true
     }),
     count(),
     tap(x => console.log(`Processed ${x} images`)),
+    takeWhile(imgCount => imgCount > 0),
+    switchMap(() => throwError('AGAIN')),
+    retry(300),
+    catchError(err => (err == 'AGAIN' ? of({}) : throwError(err))),
   );
+
+export const tagUntaggedImages = (deps: Neo4jdeps) =>
+  tagUntaggedImagesOfEntity(deps)(hikeQuery);
