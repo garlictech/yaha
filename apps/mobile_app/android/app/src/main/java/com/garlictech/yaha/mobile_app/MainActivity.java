45bc52e7f28bb7431897c8e23a9eb845ed496054
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2024-01-20 02:14:45 +0100

    fix: rename mobile_app to yaha

--- apps/mobile_app/android/app/src/main/java/com/garlictech/yaha/mobile_app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.garlictech.yaha2;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ apps/yaha/android/app/src/main/java/com/garlictech/yaha2/yaha/MainActivity.java
@@ -0,0 +1,6 @@
+package com.garlictech.yaha2.yaha;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
